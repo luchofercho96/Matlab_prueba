e243ed4b3aaab1930e41393f81de8898f443816e
luchofercho96 <49740356+luchofercho96@users.noreply.github.com>
2020-05-01 10:28:36 -0500

    cambios para comunicacion

--- Lectura_memoria.m
+++ Lectura_memoria.m
@@ -11,3 +11,11 @@ retInt = calllib('smClient64','getInt','Memory0',1)
 
 % liberar memoria compartida
 calllib('smClient64','freeMemories')
+t=0:0.1:500;
+x=0;
+for k=1:length(t)
+    x=x+1;
+    calllib('smClient64','setInt','menEntero',0,x)
+    pause(0.1);
+end
+calllib('smClient64','freeMemories')
